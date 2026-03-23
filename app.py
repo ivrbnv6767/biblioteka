489742063817483820dfb44676b5135534c69328
ivrbnv6767 <ivrbnv1@gmail.com>
2026-03-23 12:22:55 +0200

    Create game gallery with add/remove features
    Implement game gallery with add and remove functionality.

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+st.title("Galeriq ot igri")
+if "igri" not in st.session_state:
+  st.session_state.igri = []
+
+st.header("Dobavi nova igra")
+name = st.text_input("ime na igra")
+description = st.text_area("Opisanie")
+image_url = st.text_input("URL na kartinka")
+
+if st.button ("dobavi"):
+  if name and description and image_url:
+    st.session_state.igri.appened({
+      "ime" : name,
+      "opisanie" : description,
+      "kartinka" : image_url
+    })
+  else:
+    st.warning("Populnete vsichki poleta")
+
+
+if st.session_state.igri:
+  names.appened(a["ime"])
+
+remove_name = st.selectbox("Premahni igra")
+if st.button("Premahni"):
+  for a in st.session_state.igri:
+    if a["ime"] == remove_name:
+      st.session_state.igri.remove(a)
+      break
+
+st.header("Galeriq")
+if st.session_state.igri:
+  cols = st.colums(3)
+  for idx, igra in enumerate(st.session_state.igri):
+    with cols[idx % 3]:
+      st.subheader(igra["ime"])
+      st.image(igra["kartina"],use_colums_width=True)
+      st.write(igra["opisanie"]
+else:
+      st.info("Galeriqta e prazna. Dobavi igra!")
+      
+
